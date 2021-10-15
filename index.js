fa874ed704c2aa7a581283979f8d5c383d1622a6
Himanshu lulla <chinululla87@gmail.com>
2021-10-15 18:45:41 +0530

    ok

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ io.on('connection',socket=>{
             userjoinedrooms[socket.id]=[{roomID:id,roomName:payload.roomName}]
         }
         socket.emit('rooms',userjoinedrooms[socket.id]);
+        const usersinroom=socketInroom[id];
+        io.to(id).emit('room data',{arr:usersinroom,roomID:id});
+        console.log(socketInroom);
+        console.log(userjoinedrooms);
+
+    
     })
     socket.on('join room',(payload)=>{
         console.log(payload);
@@ -35,8 +41,8 @@ io.on('connection',socket=>{
         }
         else{
             socket.join(payload.roomjoined);
+            socket.broadcast.to(payload.roomjoined).emit('message', { user: 'admin', text:`${payload.name} has joined the chat`,id:null,roomID:payload.roomjoined });
             const roomtobejoined=socketInroom[payload.roomjoined][0].roomName;
-            
             socketInroom[payload.roomjoined].push({
                 id:socket.id,
                 name:payload.name,
@@ -49,14 +55,39 @@ io.on('connection',socket=>{
                 userjoinedrooms[socket.id]=[{roomID:payload.roomjoined,roomName:roomtobejoined}]
             }
             socket.emit('rooms',userjoinedrooms[socket.id]);
+            const usersinroom=socketInroom[payload.roomjoined];
+            io.to(payload.roomjoined).emit('room data',{arr:usersinroom,roomID:payload.roomjoined});
 
            
         }
 
     })
-    socket.on('get room data',(id)=>{
-        socket.emit('room data',socketInroom[id]);
-        
+ 
+    socket.on('send message',payload=>{
+        if(payload.isChannel){
+            io.to(payload.roomID).emit('message', { user: payload.sender, text: payload.message,id:socket.id,roomID:payload.roomID });
+        }
+        else{
+            socket.to(payload.roomID).emit('message',{ user: payload.sender, text: payload.message,id:socket.id,roomID:payload.senderId })
+            io.to(payload.senderId).emit('message',{ user: payload.sender, text: payload.message,id:socket.id,roomID:payload.roomID })
+        }
+           
+
+    })
+    socket.on('leave room',payload=>{
+        socket.leave(payload.roomID);
+        const newusers=userjoinedrooms[payload.myid].filter((curele)=>{
+            return curele.roomID!=payload.roomID
+        })
+       userjoinedrooms[payload.myid]=[...newusers];
+       socket.emit('rooms',userjoinedrooms[socket.id]);
+       socket.broadcast.to(payload.roomID).emit('message', { user: 'admin', text:`${payload.name} has left the chat`,id:null,roomID:payload.roomID });
+       const socketsrecord=socketInroom[payload.roomID].filter((curele)=>{
+        return curele.id!=payload.myid
+    })
+        socketInroom[payload.roomID]=[...socketsrecord];
+       const usersinroom=socketInroom[payload.roomID];
+       socket.broadcast.to(payload.roomID).emit('room data',{arr:usersinroom,roomID:payload.roomID});
     })
 
 
